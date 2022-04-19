f9f23facf8a1ab6350729a463cadd6629b2ea9c1
tabo <octavio_torrs@outlook.es>
2022-04-19 12:08:21 -0500

    client.py PEP8

--- client.py
+++ client.py
@@ -1,18 +1,31 @@
-""" Este es un intento de un docstring """
+"""Modulo cliente para establecer conexion con el launcher
 
+	Methods
+	-------
+	connect_to_launcher()
+		Actualiza el estado del script y obtiene los headers para 
+		interactuar con el endpoint
+	connecting_rtsp()
+		Establece el estado del script a CONNECTING
+	connected_rtsp()
+		Restablece el estado del script a RUNNING
+"""
 import os
 import requests
+
 import utils_launcher.data_scripts as data
 from utils_launcher.values import name_db, status, HTTP_ERROR
 from utils_launcher.credentials import USERNAME, PASSWORD, endpoint
 
-# Obtenemos el process identification number (PID) del script que invoco al cliente.
-# El PID es la forma en la que Launcher distingue todos los scripts que se 
-# estan corriendo   
+
+# Obtenemos el process identification number (PID) del script que invoco 
+# al cliente.El PID es la forma en la que Launcher distingue todos los 
+# scripts que se estan corriendo   
 pid = os.getpid()
 
 session = requests.Session() 
 headers = {}
+
 
 def connect_to_launcher():
 	"""Actulizar el estado del script a RUNNING y obtiene los headers"""
@@ -21,10 +34,11 @@ def connect_to_launcher():
 
 	# Verificamos que el servidor fue alcanzado
 	if not req:
-		raise ConnectionError(f"La conexion con login no ha podido ser establecida {req}")
+		raise ConnectionError(f"La conexion con login no ha podido \
+								ser establecida {req}")
 
 	req_json = req.json()
-	if req_json['code'] == HTTP_ERROR.UNAUTHORIZED: # Credenciales invalidas
+	if req_json['code'] == HTTP_ERROR.UNAUTHORIZED:
 		raise ConnectionError(f"Las credenciales son incorrectas")
 
 	headers['Authorization']  = 'Bearer ' + req_json['response']['token']
@@ -32,22 +46,16 @@ def connect_to_launcher():
 
 
 def update_params(coordinate_x, coordinate_y, temp):
-	"""Post EndPoint"""
+	pass
 
-	data = {"device_id": 3,
-			"coordinates": [{"x": coordinate_x,
-							 "y": coordinate_y,
-							 "temp": temp}]}
-	
-	req = session.post("http://192.168.0.135:8000/api/v1/device-coord",
-					   json=data,
-					   headers=headers)
 
 def connecting_rtsp():
 	data.update_status(name_db, pid, status.CONNECTING)
 
+
 def connected_rtsp():
 	data.update_status(name_db, pid, status.RUNNING)
+
 
 def close():
 	session.close()
