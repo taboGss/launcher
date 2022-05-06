928559bff79f419554aa9e0e630a8396ceb0a5e4
tabo <octavio_torrs@outlook.es>
2022-05-06 14:36:35 -0500

    nuevo endpoint para velocidad

--- utils_launcher/credentials.py
+++ utils_launcher/credentials.py
@@ -3,7 +3,7 @@ PASSWORD = 'secret123'
 
 class endpoint:
     LOGIN = 'http://192.168.0.135:8000/api/v1/login'
-    POST_SPEED = 'http://192.168.0.135:8000/api/v1/car-speed'
+    POST_SPEED = 'http://127.0.0.1:8000/api/v1/device-event/6'
     
     GET_DEVICES = 'http://192.168.0.135:8000/api/v1/devices'
     GET_DEVICES_ZONES = 'http://192.168.0.135:8000/api/v1/device-zones/'
